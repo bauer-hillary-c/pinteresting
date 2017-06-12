722ebb45a4ae4cb92e798fdcb0a5d2e79a7507ee
bauer-hillary-c <hbauer@ezcater.com>
2017-06-12 17:10:49 -0400

    create pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603144514) do
+ActiveRecord::Schema.define(version: 20170612210809) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
